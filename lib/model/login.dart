8734e7237f0e514776119bf8686351ede16a92ec
Abdelhady <shalsh902@gmail.com>
2023-06-05 14:03:12 +0200

    Add Profile Screen

--- lib/model/login.dart
+++ lib/model/login.dart
@@ -1,76 +1,40 @@
 class LoginModel {
   String? accessToken;
-  int? statusCode;
-  String? message;
   Content? content;
 
-  LoginModel({this.accessToken, this.statusCode, this.message, this.content});
+  LoginModel({this.accessToken, this.content});
 
   LoginModel.fromJson(Map<String, dynamic> json) {
     accessToken = json['access_token'];
-    statusCode = json['status_code'];
-    message = json['message'];
     content =
     json['content'] != null ? new Content.fromJson(json['content']) : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['access_token'] = this.accessToken;
-    data['status_code'] = this.statusCode;
-    data['message'] = this.message;
-    if (this.content != null) {
-      data['content'] = this.content!.toJson();
-    }
-    return data;
   }
 }
 
 class Content {
-  Customer? customer;
+  User? user;
 
-  Content({this.customer});
+  Content({this.user});
 
   Content.fromJson(Map<String, dynamic> json) {
-    customer = json['customer'] != null
-        ? new Customer.fromJson(json['customer'])
-        : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.customer != null) {
-      data['customer'] = this.customer!.toJson();
-    }
-    return data;
+    user = json['user'] != null ? new User.fromJson(json['user']) : null;
   }
 }
 
-class Customer {
-  int? cuId;
-  String? cuName;
-  String? cuEmail;
-  String? cuRole;
-  String? cuCreatedOn;
+class User {
+  int? uId;
+  String? uName;
+  String? uEmail;
+  String? uGender;
+  String? uRole;
 
-  Customer(
-      {this.cuId, this.cuName, this.cuEmail, this.cuRole, this.cuCreatedOn});
+  User({this.uId, this.uName, this.uEmail, this.uGender, this.uRole});
 
-  Customer.fromJson(Map<String, dynamic> json) {
-    cuId = json['cu_id'];
-    cuName = json['cu_name'];
-    cuEmail = json['cu_email'];
-    cuRole = json['cu_role'];
-    cuCreatedOn = json['cu_created_on'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['cu_id'] = this.cuId;
-    data['cu_name'] = this.cuName;
-    data['cu_email'] = this.cuEmail;
-    data['cu_role'] = this.cuRole;
-    data['cu_created_on'] = this.cuCreatedOn;
-    return data;
+  User.fromJson(Map<String, dynamic> json) {
+    uId = json['u_id']??'';
+    uName = json['u_name']??'';
+    uEmail = json['u_email']??'';
+    uGender = json['u_gender']??'';
+    uRole = json['u_role']??'';
   }
 }
