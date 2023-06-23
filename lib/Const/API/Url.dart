3eb12d8ce00c5d52e26359c7d263c9ffc056eb9a
Abdelhady <shalsh902@gmail.com>
2023-06-23 22:38:06 +0300

    Add Local Base Url

--- lib/Const/API/Url.dart
+++ lib/Const/API/Url.dart
@@ -1,7 +1,8 @@
 class ApiUrl {
-
   static const String base = "https://bookstore-api-9pzk.onrender.com/";
+  //static const String base = "http://192.168.11.102:8005/";
   static const String photoBase = "https://bookstore-api-9pzk.onrender.com";
+  //static const String photoBase = "http://192.168.11.102:8005";
   static const String login = "auth";
   static const String signup = "auth/signup";
   static const String forgetPassword = "auth/forget-password";
